8691e9cb582177e156505709064199af4bb9e503
Isabel-tech <isabelmeirabarreto@gmail.com>
2024-10-06 16:52:21 -0300

    terminei de fazer alterações

---
+++ main.py
@@ -0,0 +1,28 @@
+from config import API_KEY
+import requests
+import json
+
+# função converter -----------------------
+
+def converter():
+	moeda_de = combo_de.get()
+	moeda_para = combo_para.get()
+	valor_converter = valor.get()
+
+	resposta =  requests.get(f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{moeda_de}/{moeda_para}')
+	dados = json.loads(resposta.text)
+	cambio = dados['conversion_rate']
+	resultado = float(valor_converter) * float(cambio)
+
+	if moeda_de == 'USD':
+		simbolo = '$'
+	elif moeda_para == 'EUR':
+		simbolo = '€'
+	else:
+		simbolo = 'R$'
+	
+	valor_equivalente = simbolo + f'{resultado:,.2f}'
+ 
+	resultado['text'] = valor_equivalente
+
+	
